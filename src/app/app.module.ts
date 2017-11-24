f3b2fd74ac12b9dd9d0b969d00ac9f1f3f761016
Akhter Syed <akhters1@niksula.hut.fi>
2017-11-24 23:42:20 +0200

    resolved stuff

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { LoginComponent } from './login/login.component';
 import { FooterComponent } from './footer/footer.component';
+import { SearchComponent } from './search.component';
+const appRoutes: Routes = [
+  { path: 'search', component: SearchComponent },
+  { path: '', component: LoginComponent },
+  { path: '**', component: AppComponent }
+];
 
 
 @NgModule({
@@ -13,10 +20,13 @@ import { FooterComponent } from './footer/footer.component';
     AppComponent,
     HeaderComponent,
     LoginComponent,
-    FooterComponent
+    FooterComponent,
+    SearchComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(
+      appRoutes    )
   ],
   providers: [],
   bootstrap: [AppComponent]
