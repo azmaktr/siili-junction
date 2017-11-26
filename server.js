5ace365a62f7c65ea16cac5ea4b414e8ec1f4cfa
Naufal Khalid <naufalkhalid1991@kielikone.fi>
2017-11-26 10:26:06 +0200

    deploying again

--- server.js
+++ server.js
@@ -13,5 +13,5 @@ app.listen(process.env.PORT || 8080);
 // For all GET requests, send back index.html
 // so that PathLocationStrategy can be used
 app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname + 'dist/index.html'));
+  res.sendFile(path.join('dist/index.html'));
 });
