406aa1f4f33ac5804e7763c667a4e8b5dfabd794
Naufal Khalid <naufalkhalid1991@kielikone.fi>
2017-11-26 10:22:30 +0200

    deploying

--- server.js
+++ server.js
@@ -1,14 +1,14 @@
 // server.js
 const express = require('express');
+const path = require('path');
 const app = express();
 // Run the app by serving the static files
 // in the dist directory
-app.use(express.static(__dirname + 'dist'));
+app.use(express.static(path.join(__dirname + 'dist')));
 // Start the app by listening on the default
 // Heroku port
 app.listen(process.env.PORT || 8080);
 // server.js
-const path = require('path');
 // ...
 // For all GET requests, send back index.html
 // so that PathLocationStrategy can be used
