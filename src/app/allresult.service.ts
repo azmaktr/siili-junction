0b6f07c992eda6dde13aed31008c52ba5537e13b
Akhter Syed <akhters1@niksula.hut.fi>
2017-11-26 03:15:59 +0200

    final commit

--- src/app/allresult.service.ts
+++ src/app/allresult.service.ts
@@ -11,18 +11,19 @@ import "rxjs/add/operator/map";
 import "rxjs/add/operator/switchMap";
 import "rxjs/add/operator/toPromise";
 const API_ENDPOINT = "http://junction-backendapp-01.azurewebsites.net/objects";
+const API_ENDPOINT_SINGLE =
+  "http://junction-backendapp-01.azurewebsites.net/objectbyid?objectid=";
 const token =
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJkYXRhIjp7InVzZXJJRCI6IjViNzBkYTY1LTlkMzQtNDQzNS1iODE3LWQ2YmU5ZGZjYTYwZCJ9LCJpYXQiOjE1MTE2MzE0NjQsImV4cCI6MTUxMTgwNDI2NH0.yHkmU9pFo0Q4mkHevJtHqhJ0Raa7tSxjlCclhR0nfyw";
 @Injectable()
 export class AllresultService {
-  constructor(private http: Http) {
-  }
+  constructor(private http: Http) {}
 
   getResults() {
     let headers = new Headers({
-      "token": token,
+      token: token,
       "content-type": "application/json",
-      "accept": "application/json"
+      accept: "application/json"
     });
     let options = new RequestOptions({ headers: headers });
     return this.http.get(API_ENDPOINT, options).map(res => res.json());
@@ -37,26 +38,36 @@ export class AllresultService {
 
   searchStuff(searchStr = "") {
     let headers = new Headers({
-      "token": token,
+      token: token,
       "content-type": "application/json",
-      "accept": "application/json"
+      accept: "application/json"
     });
     let options = new RequestOptions({ headers: headers });
     return this.http.get(API_ENDPOINT, options).map(res => {
       return this.search(searchStr, res.json());
     });
-    
   }
 
   searchQty(searchStr = "", qty = 4) {
     let headers = new Headers({
-      "token": token,
+      token: token,
       "content-type": "application/json",
-      "accept": "application/json"
+      accept: "application/json"
     });
     let options = new RequestOptions({ headers: headers });
     return this.http.get(API_ENDPOINT, options).map(res => {
-      return this.search(searchStr, res.json()).slice(0,qty);
+      return this.search(searchStr, res.json()).slice(0, qty);
     });
+  }
+  searchOne(searchId = "") {
+    let headers = new Headers({
+      token: token,
+      "content-type": "application/json",
+      accept: "application/json"
+    });
+    let options = new RequestOptions({ headers: headers });
+    return this.http
+      .get(API_ENDPOINT_SINGLE + searchId, options)
+      .map(res => res.json());
   }
 }
--- src/app/product/product.component.ts
+++ src/app/product/product.component.ts
@@ -1,15 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { Router, ActivatedRoute, Params } from "@angular/router";
+import { AllresultService } from "../allresult.service";
+import { Route } from "@angular/router/src/config";
 
 @Component({
-  selector: 'app-product',
-  templateUrl: './product.component.html',
-  styleUrls: ['./product.component.css']
+  selector: "app-product",
+  templateUrl: "./product.component.html",
+  styleUrls: ["./product.component.css"]
 })
 export class ProductComponent implements OnInit {
-
-  constructor() { }
+  result;
+  searchId;
+  constructor(
+    private results: AllresultService,
+    private activatedRoute: ActivatedRoute
+  ) {}
 
   ngOnInit() {
+    this.activatedRoute.params.subscribe((params: Params) => {
+      this.searchId = params["id"];
+      this.results.searchOne(this.searchId).subscribe(result => {
+        this.result = result.pop();
+      });
+    });
   }
-
 }
