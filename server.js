6bb6b85e3db6f3487d3817af9149c56eacd4ab10
Naufal Khalid <naufalkhalid1991@kielikone.fi>
2017-11-26 04:04:03 +0200

    dist

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 // Run the app by serving the static files
 // in the dist directory
-app.use(express.static(__dirname + '/dist'));
+app.use(express.static(__dirname + '/'));
 // Start the app by listening on the default
 // Heroku port
 app.listen(process.env.PORT || 8080);
@@ -13,5 +13,5 @@ const path = require('path');
 // For all GET requests, send back index.html
 // so that PathLocationStrategy can be used
 app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname + '/dist/index.html'));
+  res.sendFile(path.join(__dirname + '/index.html'));
 });
